2e72859aa64b400fc1152d651bd2241ba45023c9
— Chichiii? <cintarahmaliyasyifa04@gmail.com>
2026-01-27 18:19:28 +0700

    first
    belajar dek

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Welcome to Java in Umm, Cinta !");
+        System.out.println("duh, dejavu banget sama java.. terakhir buka java tahun 2022 :((");
+        }
+    }
